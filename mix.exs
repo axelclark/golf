8afef7a77c65760b0e0e447249905e59eb5858ce
Axel Clark <axelclark2@yahoo.com>
2018-11-25 19:26:53 -0500

    Add Distillery for releases on gigalixir

--- mix.exs
+++ mix.exs
@@ -33,15 +33,16 @@ defmodule Golf.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.0"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.0"},
+      {:distillery, "~> 2.0"},
       {:ecto_sql, "~> 3.0"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix, "~> 1.4.0"},
+      {:phoenix_ecto, "~> 4.0"},
+      {:phoenix_html, "~> 2.11"},
+      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:plug_cowboy, "~> 2.0"}
     ]
   end
