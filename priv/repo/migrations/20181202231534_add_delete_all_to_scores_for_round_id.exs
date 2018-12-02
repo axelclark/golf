e2c67906c090e196eeba5f78a0b5e3e723ddbad2
Axel Clark <axelclark2@yahoo.com>
2018-12-02 18:56:36 -0500

    Fix migration bug
    * Fix migration bug where Ecto doesn't know how to handle rollback
    * Closes #27

--- priv/repo/migrations/20181202231534_add_delete_all_to_scores_for_round_id.exs
+++ priv/repo/migrations/20181202231534_add_delete_all_to_scores_for_round_id.exs
@@ -1,10 +1,17 @@
 defmodule Golf.Repo.Migrations.AddDeleteAllToScoresForRoundId do
   use Ecto.Migration
 
-  def change do
+  def up do
     execute "ALTER TABLE scores DROP CONSTRAINT scores_round_id_fkey"
     alter table(:scores) do
       modify :round_id, references(:rounds, on_delete: :delete_all)
+    end
+  end
+
+  def down do
+    execute "ALTER TABLE scores DROP CONSTRAINT scores_round_id_fkey"
+    alter table(:scores) do
+      modify :round_id, references(:rounds, on_delete: :nothing)
     end
   end
 end
--- priv/repo/migrations/20181202232729_add_delete_all_to_holes_for_course_id.exs
+++ priv/repo/migrations/20181202232729_add_delete_all_to_holes_for_course_id.exs
@@ -1,10 +1,17 @@
 defmodule Golf.Repo.Migrations.AddDeleteAllToHolesForCourseId do
   use Ecto.Migration
 
-  def change do
+  def up do
     execute "ALTER TABLE holes DROP CONSTRAINT holes_course_id_fkey"
     alter table(:holes) do
       modify :course_id, references(:courses, on_delete: :delete_all)
+    end
+  end
+
+  def down do
+    execute "ALTER TABLE holes DROP CONSTRAINT holes_course_id_fkey"
+    alter table(:holes) do
+      modify :course_id, references(:courses, on_delete: :nothing)
     end
   end
 end
