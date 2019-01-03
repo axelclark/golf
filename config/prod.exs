cad4199dfea966f4d6b1d002bd3947442093094f
Axel Clark <axelclark2@yahoo.com>
2019-01-03 18:19:04 -0500

    Update config/prod.exs to fix url
    * Update config/prod.exs to fix url
    * Link has port at end of url
    * Closes #79

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :golf, GolfWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "golf.gigalixirapp.com", port: 80],
+  url: [scheme: "https", host: "golf.gigalixirapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
