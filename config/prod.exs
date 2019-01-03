296552f1c1c791083f978360fd703cd94a4357a4
Axel Clark <axelclark2@yahoo.com>
2019-01-03 07:39:51 -0500

    Add https to url in config.prod.exs
    * Add https to url in config.prod.exs
    * Reset password used http rather than https
    * Closes #77

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :golf, GolfWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "golf.gigalixirapp.com", port: 80],
+  url: [scheme: "https", host: "golf.gigalixirapp.com", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   secret_key_base: "${SECRET_KEY_BASE}"
